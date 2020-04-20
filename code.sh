ecafbe7e1b775a3f6d0bdff62f7a93c651c3fca3
Filippe LeMarchand <gasinvein@gmail.com>
2020-04-20 15:54:43 +0300

    Add extension point for tools

--- code.sh
+++ code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+shopt -s nullglob
 
 FIRST_RUN="${XDG_CONFIG_HOME}/flatpak-vscode-first-run"
 
@@ -12,6 +13,30 @@ if [ ! -f ${FIRST_RUN} ]; then
   WARNING_FILE="/app/share/vscode/flatpak-warning.txt"
   touch ${FIRST_RUN}
 fi
+
+PYTHON_SITEDIR=$(python3 <<EOFPYTHON
+import os
+import site
+print(os.path.relpath(site.getusersitepackages(), site.getuserbase()))
+EOFPYTHON
+)
+
+for tool_dir in /app/tools/*; do
+  tool_bindir=$tool_dir/bin
+  if [ -d "$tool_bindir" ]; then
+    msg "Adding $tool_bindir to PATH"
+    export PATH=$PATH:$tool_bindir
+  fi
+  tool_pythondir=$tool_dir/$PYTHON_SITEDIR
+  if [ -d "$tool_pythondir" ]; then
+    msg "Adding $tool_pythondir to PYTHONPATH"
+    if [ -z "$PYTHONPATH" ]; then
+      export PYTHONPATH=$tool_pythondir
+    else
+      export PYTHONPATH=$PYTHONPATH:$tool_pythondir
+    fi
+  fi
+done
 
 if [ "$FLATPAK_ENABLE_SDK_EXT" = "*" ]; then
   SDK=()
